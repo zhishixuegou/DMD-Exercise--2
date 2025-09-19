90d0cb3921ad8b60d89735efa4d5e29afdc0d1c7
prabhatram <prabhat.ram@oulu.fi>
2025-09-19 14:48:54 +0800

    exercise 2 graded

---
+++ exercise_2_graded.sql
@@ -0,0 +1,96 @@
+/* Create a table medication_stock in your Smart Old Age Home database. The table must have the following attributes:
+ 1. medication_id (integer, primary key)
+ 2. medication_name (varchar, not null)
+ 3. quantity (integer, not null)
+ Insert some values into the medication_stock table. 
+ Practice SQL with the following:
+ */
+
+ -- Q!: List all patients name and ages 
+
+
+ -- Q2: List all doctors specializing in 'Cardiology'
+
+
+ 
+ -- Q3: Find all patients that are older than 80
+
+
+
+
+-- Q4: List all the patients ordered by their age (youngest first)
+
+
+
+
+-- Q5: Count the number of doctors in each specialization
+
+
+
+-- Q6: List patients and their doctors' names
+
+
+
+-- Q7: Show treatments along with patient names and doctor names
+
+
+
+-- Q8: Count how many patients each doctor supervises
+
+
+
+-- Q9: List the average age of patients and display it as average_age
+
+
+
+-- Q10: Find the most common treatment type, and display only that
+
+
+
+-- Q11: List patients who are older than the average age of all patients
+
+
+
+-- Q12: List all the doctors who have more than 5 patients
+
+
+
+
+-- Q13: List all the treatments that are provided by nurses that work in the morning shift. List patient name as well. 
+
+
+
+
+-- Q14: Find the latest treatment for each patient
+
+
+
+-- Q15: List all the doctors and average age of their patients
+
+
+
+-- Q16: List the names of the doctors who supervise more than 3 patients
+
+
+
+-- Q17: List all the patients who have not received any treatments (HINT: Use NOT IN)
+
+
+
+
+-- Q18: List all the medicines whose stock (quantity) is less than the average stock
+
+
+
+
+-- Q19: For each doctor, rank their patients by age
+
+
+
+-- Q20: For each specialization, find the doctor with the oldest patient
+
+
+
+
+
+
